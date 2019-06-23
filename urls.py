ac5ac6703f37ecc26b614be60ec62be75e0ad62c
seoyeon1 <30707885+seoyeon1@users.noreply.github.com>
2019-06-24 07:40:01 +0900

    app(accounts) urls upload
    accounts/urls.py

--- urls.py
+++ urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from . import views     #import portfolio.views 같은 의미.
+from . import views     #import accounts.views 같은 의미.
 
 urlpatterns = [     #이 app에서 다루는 path들 (code를 간결하게 관리)
-    path('portfolio/', views.portfolio, name='portfolio'),
+
+    path('signup/', views.signup, name='signup'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
 ]
